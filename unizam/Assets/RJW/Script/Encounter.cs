aa64b211f4c0030cdbf5875213dc5bdb7ffd05de
roxasventus <magicwand10@naver.com>
2025-01-12 00:22:14 +0900

    이동씬 완성

--- unizam/Assets/RJW/Script/Encounter.cs
+++ unizam/Assets/RJW/Script/Encounter.cs
@@ -26,6 +26,10 @@ public class Encounter : MonoBehaviour
     public Image fadeImage; // 페이드 효과 이미지
     public List<Sprite> changeImages; // 변경할 스프라이트 목록
 
+    public AudioClip encounterAudio;
+    //1 - 두억 등장, 2- 구미호 등장, 3- 이무기 등장
+    public AudioSource audioPlay;
+
     void Start()
     {
     }
@@ -39,8 +43,10 @@ public class Encounter : MonoBehaviour
                     StartCoroutine(duFade());
                     break;
                 case monster.gu:
+                    StartCoroutine(guFade());
                     break;
                 case monster.snake:
+                    StartCoroutine(snakeFade());
                     break;
                 case monster.man:
                     StartCoroutine(manFade());
@@ -56,6 +62,9 @@ public class Encounter : MonoBehaviour
         {
             Debug.Log("방문");
             isVisit = true;
+            if (monster == monster.snake) {
+                StartCoroutine(snakeFade());
+            }
         }
     }
 
@@ -80,6 +89,10 @@ public class Encounter : MonoBehaviour
         if (monster != null)
         {
             monster.gameObject.SetActive(true);
+            if (encounterAudio != null) { 
+                audioPlay.clip = encounterAudio;
+                audioPlay.Play();
+            }
 
             Image monsterImage = monster.GetComponent<Image>();
             if (monsterImage != null && changeImages.Count > 0)
@@ -211,6 +224,75 @@ public class Encounter : MonoBehaviour
 
     //////////////////////////
 
+    // gu ///////////////////////////////////////////////
+
+    IEnumerator guFade()
+    {
+        Transform monster = fadeImage.transform.Find("gumiho");
+
+        if (monster != null)
+        {
+            monster.gameObject.SetActive(true);
+
+            if (encounterAudio != null)
+            {
+                audioPlay.clip = encounterAudio;
+                audioPlay.Play();
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            // Image 컴포넌트 가져오기
+            Image monsterImage = monster.GetComponent<Image>();
+            if (monsterImage != null)
+            {
+                // 색상 변경 시작
+                yield return StartCoroutine(FadeToBlack(monsterImage));
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+
+
+
+    //////////////////////////
+
+    // snake ///////////////////////////////////////////////
+
+    IEnumerator snakeFade()
+    {
+        Transform monster = fadeImage.transform.Find("emugi");
+
+        if (monster != null)
+        {
+            monster.gameObject.SetActive(true);
+
+            if (encounterAudio != null)
+            {
+                audioPlay.clip = encounterAudio;
+                audioPlay.Play();
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            // Image 컴포넌트 가져오기
+            Image monsterImage = monster.GetComponent<Image>();
+            if (monsterImage != null)
+            {
+                // 색상 변경 시작
+                yield return StartCoroutine(FadeToBlack2(monsterImage));
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+
+
+
+    //////////////////////////
 
 
     IEnumerator FadeToBlack(Image monsterImage)
@@ -235,6 +317,28 @@ public class Encounter : MonoBehaviour
 
     }
 
+    IEnumerator FadeToBlack2(Image monsterImage)
+    {
+        Color initialColor = monsterImage.color;
+        Color targetColor = Color.black; // 검은색으로 변경
+
+        float transitionTime = 1.0f; // 색상 전환 시간
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionTime)
+        {
+            elapsedTime += Time.deltaTime;
+            monsterImage.color = Color.Lerp(initialColor, targetColor, elapsedTime / transitionTime);
+            yield return null;
+        }
+
+        monsterImage.color = targetColor;
+
+        yield return new WaitForSeconds(0.3f);
+        MoveBossScnene();
+
+    }
+
     public void MoveScnene()
     {
         for (int i = 0; i < enemy.Count; i++)
@@ -246,6 +350,20 @@ public class Encounter : MonoBehaviour
         GameManager.Instance.Reward = reward;
 
         SceneManager.LoadScene("BattleScene");
+
+    }
+
+    public void MoveBossScnene()
+    {
+        for (int i = 0; i < enemy.Count; i++)
+        {
+            GameManager.Instance.enemies.Add(enemy[i]);
+        }
+        GameManager.Instance.bg = bg;
+        GameManager.Instance.monster = monster;
+        GameManager.Instance.Reward = reward;
+
+        SceneManager.LoadScene("BossScene");
 
     }
 
