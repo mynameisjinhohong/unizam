285328bc91105c6667c9c314b4e20be40dca6e6f
mynameisjinhohong <ghddhksduq@naver.com>
2025-01-12 06:07:57 +0900

    버프 설명 위치 수정

--- unizam/Assets/HJH/Script/BuffMove.cs
+++ unizam/Assets/HJH/Script/BuffMove.cs
@@ -4,14 +4,19 @@ public class BuffMove : MonoBehaviour
 {
     void Start()
     {
-        // UI 요소의 월드 좌표를 화면 뷰포트 좌표로 변환
-        Vector3 viewportPos = Camera.main.WorldToViewportPoint(gameObject.GetComponent<RectTransform>().position);
 
-        // 화면 왼쪽에 있는 경우 (뷰포트 좌표의 x 값이 0.5보다 작음)
+        // 화면 픽셀 좌표를 뷰포트 좌표(0~1)로 변환
+        Vector3 viewportPos = Camera.main.ScreenToViewportPoint(transform.position);
+        // 화면의 왼쪽에 있는 경우 (뷰포트 좌표의 x 값이 0.5보다 작음)
         if (viewportPos.x > 0.5f)
         {
             // 자식 오브젝트의 위치 변경
             transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(-240, 0);
+        }
+        else
+        {
+
+            transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
         }
     }
 }
