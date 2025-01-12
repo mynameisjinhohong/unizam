5c9e55b0a43ff6759484b2471b8cfb33b9178b5a
mynameisjinhohong <ghddhksduq@naver.com>
2025-01-12 09:22:31 +0900

    인트로 만드는 중

---
+++ unizam/Assets/HJH/Script/Intro.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public Image sprite;
+    int idx = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            idx++;
+
+        }
+    }
+}
